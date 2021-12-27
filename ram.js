48b896ab3f2ad7231155eec35ed7b8fccfc1c714
yubraja <yubrajadhikari987@gmail.com>
2021-12-27 23:40:48 +0545

    2nd time push

---
+++ ram.js
@@ -0,0 +1,21 @@
+/*function fact(n)
+{
+    var facto=1;
+    for(i=1;i<=n;i++)
+    {
+        facto=facto*i;
+    }
+    return facto;
+}
+var a= window.prompt("enter a number");
+var factorial= fact(a);
+console.log("sum=",factorial);*/
+function multi(n)
+{
+    for (i=1;i<=10;i++)
+    {
+        document.write("",a,"*",i,"=",a*i,"<br>");
+    }
+}
+var a =window.prompt("enter a number");
+multi(a);
